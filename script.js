fdfff5ebd93483c8db6356c1a493d42fbd19ba57
Markuzans <119396400+Markuzans@users.noreply.github.com>
2023-04-11 10:09:29 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,23 +2,31 @@
 // console.log(kaķis)
 
 const mīnuss = document.querySelector('.js-minus');
-console.log(mīnuss);
-const plusins = document.querySelector('.js-pluss');
-console.log(plusins);
+const plusinš = document.querySelector('.js-pluss');
 const reizina = document.querySelector('.js-multiply');
-console.log(reizina);
 const dalīšana = document.querySelector('.js-divide');
 console.log(dalīšana);
+console.log(reizina);
+console.log(plusinš);
+console.log(mīnuss);
 
-const input = document.querySelector('.js-input');
+const input1 = document.querySelector(".js-input1")
+const input2 = document.querySelector(".js-input2")
+
+const atbilde = document.querySelector('.js-result')
 
 const Add = (pirmais, otrais) => {return pirmais + otrais}
 const minus = (pirmais, otrais) => {return pirmais - otrais}
 const divide = (pirmais, otrais) => {return pirmais / otrais}
 const multiply = (pirmais, otrais) => {return pirmais * otrais}
-console.log(multiply(4, 8))
-console.log(divide(4, 8))
-console.log(minus(4, 8))
-console.log(Add(4, 8))
+//console.log(multiply(4, 8))
+//console.log(divide(4, 8))
+//console.log(minus(4, 8))
+//console.log(Add(4, 8))
 
-plusins.addEventListener('click', () => {console.log("Mani nospieda")})
+plusinš.addEventListener('click', () => { answer.textContext = Add(parseInt(input1.value), parseInt(input2.value))
+
+})
+mīnuss.addEventListener('click', () => {console.log(minus(parseInt(input1.value), parseInt(input2.value)))})
+reizina.addEventListener('click', () => {console.log(multiply(parseInt(input1.value), parseInt(input2.value)))})
+dalīšana.addEventListener('click', () => {console.log(divide(parseInt(input1.value), parseInt(input2.value)))})
